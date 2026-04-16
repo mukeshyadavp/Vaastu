2a892e6c233103d69756d5bca6ffab7909225375
HARISHYADAV8688 <harishyadav2905@gmail.com>
2026-04-16 14:13:04 +0530

    added images governance dashboard

--- src/Components/GovernanceDashboard.tsx
+++ src/Components/GovernanceDashboard.tsx
@@ -1,5 +1,9 @@
 import ApplicationsTable from "../Components/ApplicationsTable";
-import mapHeader from "../assets/map1.png";
+import map1 from "../assets/map1.png";
+import map2 from "../assets/ds-image2.png";
+import map3 from "../assets/ds-image3.png";
+import map4 from "../assets/ds-image4.jpg";
+import "./GovernanceDashboard.css";
 
 const GovernanceDashboard = ({ applications,  onApprove, onReject }: any) => {
   return (
@@ -7,7 +11,12 @@ const GovernanceDashboard = ({ applications,  onApprove, onReject }: any) => {
       <h2>Governance Dashboard</h2>
 
       
-        <img src={mapHeader} alt="Map Header" className="map-header-img" />
+     <div className="map-grid">
+  <img src={map1} alt="Map1" />
+  <img src={map2} alt="Map2" />
+  <img src={map3} alt="Map3" />
+  <img src={map4} alt="Map4" />
+</div>
       
       <ApplicationsTable
         data={applications}
