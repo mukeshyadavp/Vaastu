13f2af6c35a818ebeeccf872c06ecec1b3517c0d
mukeshyadavp <pmukeshyadav12@gmail.com>
2026-04-17 08:31:54 +0530

    Chatbot added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import AdminDashboard from "./Components/AdminDashboard";
+import ChatbotWidget from "./Components/ChatbotWidget";
 import Home from "./Components/pages/Home";
 import Dashboard from "./Components/pages/Dashboard";
 
 function App() {
   return (
-    <Routes>
+    <>
+      <Routes>
         <Route path="/" element={<Home />} />
-      <Route path="/Home" element={<Home />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+
+      <ChatbotWidget />
+    </>
   );
 }
 
